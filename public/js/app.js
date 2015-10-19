00cce71a6ba0deb437e635897b5f3248f6df5355
Aladdin Sonni <alaaden3000@gmail.com>
2015-10-19 13:50:44 +0200

    #4

--- public/js/app.js
+++ public/js/app.js
@@ -4,15 +4,15 @@ var weatherApp = angular.module('weatherApp', ['ngRoute','ngResource']);
 weatherApp.config(function($routeProvider) {
   $routeProvider
   .when('/', {
-    templateUrl: 'pages/home.htm',
+    templateUrl: 'pages/home.html',
     controller: 'homeController'
   })
   .when('/forecast', {
-    templateUrl: 'pages/forecast.htm',
+    templateUrl: 'pages/forecast.html',
     controller: 'forecastController'
   })
   .when('/forecast/:days', {
-    templateUrl: 'pages/forecast.htm',
+    templateUrl: 'pages/forecast.html',
     controller: 'forecastController'
   })
 });
